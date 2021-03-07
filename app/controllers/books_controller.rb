3c0f72ab7e4caed6a2c91f741caf7a386d13fcc7
massive2351 <massive2351@gmail.com>
2021-03-07 05:30:51 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,39 @@
 class BooksController < ApplicationController
   
   def index
-  end
-
-  def show
-  end
-
-  def new
+    @books = Book.all
     @book = Book.new
-  end
-
-  def edit
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to books_path(@book)
+    else
+      render :new
+    end
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(@book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(@book)
+  end
+  
   def top
   end
   
@@ -26,4 +41,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
